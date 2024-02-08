3b270e355ca534310fdb5d75b4d971e12e884103
cmleahy13 <cleahy@uvm.edu>
2024-02-07 21:53:38 -0500

    Questions

--- test_plot_p300_erps.py
+++ test_plot_p300_erps.py
@@ -57,23 +57,27 @@ for subject_index in range(3,11):
     
 """
 
-1. Repeated up/down patterns of channels?
+1. Why do we see repeated up-and-down patterns on many of the channels?
    
-    Brainwaves have a general set frequency (for multiple waves that sum, each at a different frequency). Consequently, there will likely be a repetitive nature of baseline activity. Variation (i.e. peaks) may occur due to some change in activity, whether related to the event itself, related thoughts, motion, or artifacts.
+    Brainwaves have a general set frequency (for multiple waves that sum, each at a different frequency). Consequently, there will likely be some repetitive nature of baseline activity. Additionally, the epochs themselves often occur in evenly spaced intervals for large periods of time, where there may often be overlap with pre- and post-event activity, suggesting that voltages before and after the events are likely not completely isolated. Variation (i.e. peaks) may occur due to some change in activity, whether related to the event itself, related thoughts, motion, or artifacts.
     
-2. Patterns more pronounced in some channels and not others?
+2. Why are they more pronounced in some channels but not others?
 
-    Consistency of the repetitive patterns may have occurred in some channels more than others due to the relevance, or lack thereof, to the assignment at hand. For example, the central channel is located in an area that may be more closely linked to motor control compared with language, so while the proximity of the electrodes causes the overall activity to likely have extensive effects, behavior that is less concerned with the task (language, counting, concentrating, etc.) may be more rhythmic.
+    Consistency may have occurred in some channels more than others due to the relevance, or lack thereof, to the assignment at hand. For example, the central channel is located in an area that may be more closely linked to motor control compared with language, so while the proximity of the electrodes causes the overall activity to likely have extensive effects, behavior that is less concerned with the task (language, counting, concentrating, etc.) may be more rhythmic.
 
-3. Why do some channels have positive peak ~0.5s after event?
+3. Why does the voltage on some of the channels have a positive peak are half a second after a target flash?
     
-    These channels may be more involved in the processing of the information presented (target or nontarget). This positive peak is most noticeable for subject 3 and occurs in channels 1, 2, 3, 5, and 6 (0-based). Subject 5 has a noticeable negative peak around 0.5 seconds after the event, however, on all channels except channel 4, which appears to be some sort of artifact. Subjects 8 and 10 also observe that negative peak around the same time for all channels except channel 7. Post-synaptic potentials, the voltage measured by EEG is measured as it serves to likely initiate (excite) an action potential. The peaks were almost always associated with the targets rather than the nontargets, suggesting the activation of neurons related to language or concentration.
+    At the foundational level, this experiment is using a P300 Speller, indicating that there is likely a "positive-going" peak around 300ms; however, the 300ms is based on the first P300 rather than what is typical, which would really be P500, suggesting the peak occurs 500ms (or 0.5s) after the event (although several of the subjects exhibit peaks roughly a quarter of a second after the event, which is consistent with the 300ms). Some reasons we could expect this behavior is that these channels may be more involved in the processing of the information presented (target or nontarget); peaks are pretty strongly limited to targets, as nontarget EEG data appears to follow a consistent pattern through the event occurrence. 
     
-4. Which EEG channels are these (ones above) and why?
+    This positive peak is most noticeable for subject 3 and occurs in channels 1, 2, 3, 5, and 6 (0-based). Subject 5 has a noticeable negative peak around 0.5 seconds after the event, however, on all channels except channel 4, which appears to be some sort of artifact. Subjects 8 and 10 also observe that negative peak around the same time for all channels except channel 7. Post-synaptic potentials, the voltage measured by EEG is measured as it serves to likely initiate (excite) an action potential. The peaks were almost always associated with the targets rather than the nontargets, suggesting the activation of neurons related to language or concentration.
+    
+4. Which EEG channels (e.g., Cz) do you think these (the ones described in the last question) might be and why?
 
-    Visual processing of the flashes likely occurred in the occipital lobe, which only corresponded to a single channel. Frontal lobe involved in concentrating/thinking, which would be necessary for the spelling to occur; only one channel was placed on the frontal lobe. Five of the channels are involved with the parietal lobe, however, which corresponds to the number of channels exhibiting peaks for subject 3, which could make sense seeing as the parietal lobe is often involved with language and attention. Because of the poor spatial resolution of EEG, many of the electrodes are likely to measure similar occurrences, and given the proximity of the parietal lobe electrodes to one another, they could be observing the behavior of the neurons near each other's electrodes.
+    Visual processing of the flashes likely occurred in the occipital lobe, which only corresponded to a single channel. The frontal lobe is involved in concentrating and thinking, which would be necessary for the spelling to occur; only one channel was placed on the frontal lobe. Five of the channels are involved with the parietal lobe, however, which corresponds to the number of channels exhibiting peaks for subject 3, which could make sense seeing as the parietal lobe is often involved with language and attention. Because of the poor spatial resolution of EEG, many of the electrodes are likely to measure similar occurrences, and given the proximity of the parietal lobe electrodes to one another, they could be observing the behavior of the neurons near each other's electrodes. Overall, it is somewhat likely that the channels that observe the peaks include P3, Pz, P4, Po7, and Po8.
+    
+5. Describe how running the code on multiple subjects influenced your answers.
 
-Using the data from all of the subjects permitted an overarching view of the general behavior. For each subject, the data produced was unique; however, some general patterns could be obsered, such as the positive or negative peaks that occurred at specific points in time relative to the event.
+    Using the data from all of the subjects permitted an overarching view of the general behavior. For each subject, the data produced was unique; however, some general patterns could be obsered, such as the positive or negative peaks that occurred at specific points in time relative to the event.
 
 """
     
